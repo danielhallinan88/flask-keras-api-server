6097c38263738f9400b7943852da2a273ac45d9a
danielhallinan88@gmail.com <danielhallinan88@gmail.com>
2019-10-06 18:51:24 +0000

    adding gunicorn logging

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # increase workers with cpu
-/home/ubuntu/venv/bin/gunicorn --bind 0.0.0.0:5000 wsgi --workers 2
+/home/ubuntu/venv/bin/gunicorn --bind 0.0.0.0:5000 wsgi --log-file=flask-keras-api.log --workers 2
